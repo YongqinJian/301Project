bf2759f170455f891187de5bd02094de3b348a62
YixianLu <yixian@ualberta.ca>
2020-02-05 19:47:56 -0700

    trapezoid

--- AndroidProject/app/src/main/java/com/example/androidproject/Trapezoid.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/Trapezoid.java
@@ -2,6 +2,10 @@ package com.example.androidproject;
 
 import android.graphics.drawable.shapes.Shape;
 
-public class Trapezoid extends Shape {
-    
+abstract public class Trapezoid extends Shape {
+    public Trapezoid(int i) {
+        this.i = i;
+    }
+
+    int i = 2;
 }
